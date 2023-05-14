76d193a4dd6ca2070bb8cfc7edce9bc979618ef7
unknown <rezacts01@gmail.com>
2023-05-14 20:50:38 +0700

    deploy

---
+++ assets/notFound-c359cacb.js
@@ -0,0 +1,1 @@
+import{c as e,j as t,R as o,a as r,r as s}from"./main-b2d0a7b4.js";const c=document.getElementById("root"),a=e.createRoot(c);a.render(t.jsx(o.StrictMode,{children:t.jsx(r,{router:s})}));
