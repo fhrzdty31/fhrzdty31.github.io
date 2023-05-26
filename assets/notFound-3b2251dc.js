b09413a0052359be1259c10a77e06f5d0981878f
unknown <rezacts01@gmail.com>
2023-05-26 07:29:40 +0700

    deploy

---
+++ assets/notFound-3b2251dc.js
@@ -0,0 +1,1 @@
+import{c as e,j as t,R as o,a as r,r as s}from"./main-be344828.js";const c=document.getElementById("root"),a=e.createRoot(c);a.render(t.jsx(o.StrictMode,{children:t.jsx(r,{router:s})}));
