0a0896c0f7cbf3ba90fdc073816f1b6b2daaa193
fhrzdty31 <rezacts01@gmail.com>
2023-06-01 10:23:03 +0700

    deploy

--- assets/notFound-78217b7a.js
+++
@@ -1,1 +0,0 @@
-import{c as e,j as t,R as o,a as r,r as s}from"./main-392335b1.js";const c=document.getElementById("root"),a=e.createRoot(c);a.render(t.jsx(o.StrictMode,{children:t.jsx(r,{router:s})}));
